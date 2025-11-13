362c538d35a4eaabb8a99d79a6631195394911b1
thenurinethangi <thenurinathangi@gmail.com>
2025-11-13 14:30:21 +0530

    develop tasks piority page

---
+++ src/components/urgentTasks.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import {Ellipsis, Plus, ChevronDown} from "lucide-react";
+
+function UrgentTasks() {
+  return (
+    <div style={{padding: '15px'}} className='shadow-xl rounded-[5px] flex flex-col gap-3'>
+        <div className='flex justify-between items-center'>
+            <div>
+                <h1 className='text-[14.5px] font-bold tracking-[0.03em] text-red-500 opacity-95'>Urgent</h1>
+            </div>
+            <div className='flex items-center gap-1.5'>
+                <Plus className="w-4.5 h-4.5 text-gray-500" />
+                <Ellipsis className="w-4.5 h-4.5 text-gray-500" />
+            </div>
+        </div>
+
+        <div>
+            <div className='flex items-center gap-1.5' style={{marginBottom: '5px'}}>
+                <ChevronDown className="w-4 h-4 text-gray-500" />
+                <p className='text-[12.8px] font-medium'>Today</p>
+                <p className='text-[12.8px] font-normal text-gray-400'>5</p>
+            </div>
+
+            {/* single tasks */}
+            <div className='flex flex-col gap-2 rounded-sm' style={{paddingTop: '8px'}}>
+                <div className='flex justify-between items-center' style={{paddingInline: '15px 5px'}}>
+                    <div className='flex items-center gap-2'>
+                        <input type='checkbox' className='w-3.5 h-3.5'></input>
+                        <p className='text-[14.5px]'>Develop TickTick Frondend</p>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <p className='text-[13.1px] text-[#4772FA]'>Today</p>
+                        <Ellipsis className="w-3 h-3 text-gray-300" />
+                    </div>
+                </div>
+                <div className='flex justify-center'>
+                    <hr className='text-[#EEEEEE] w-[90%]'></hr>
+                </div>
+            </div>
+
+            <div className='flex flex-col gap-2 rounded-sm' style={{paddingTop: '8px'}}>
+                <div className='flex justify-between items-center' style={{paddingInline: '15px 5px'}}>
+                    <div className='flex items-center gap-2'>
+                        <input type='checkbox' className='w-3.5 h-3.5'></input>
+                        <p className='text-[14.5px]'>Develop TickTick Frondend</p>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <p className='text-[13.1px] text-[#4772FA]'>Today</p>
+                        <Ellipsis className="w-3 h-3 text-gray-300" />
+                    </div>
+                </div>
+                <div className='flex justify-center'>
+                    <hr className='text-[#EEEEEE] w-[90%]'></hr>
+                </div>
+            </div>
+
+            <div className='flex flex-col gap-2 rounded-sm' style={{paddingTop: '8px'}}>
+                <div className='flex justify-between items-center' style={{paddingInline: '15px 5px'}}>
+                    <div className='flex items-center gap-2'>
+                        <input type='checkbox' className='w-3.5 h-3.5'></input>
+                        <p className='text-[14.5px]'>Develop TickTick Frondend</p>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <p className='text-[13.1px] text-[#4772FA]'>Today</p>
+                        <Ellipsis className="w-3 h-3 text-gray-300" />
+                    </div>
+                </div>
+                <div className='flex justify-center'>
+                    <hr className='text-[#EEEEEE] w-[90%]'></hr>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <p className='text-[12px] text-[#9E9E9E] relative left-2'>View More</p>
+        </div>
+    </div>
+  )
+}
+
+export default UrgentTasks
--- src/pages/tasksStatusPage.tsx
+++ src/pages/tasksStatusPage.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import NavBar from '../components/navBar'
+import UrgentTasks from '../components/urgentTasks'
+import NotUrgentTasks from '../components/notUrgentTasks'
+import ImportantTasks from '../components/importantTasks'
+import UnimportantTasks from '../components/unimportantTasks'
 
 function TasksStatusPage() {
   return (
-    <div>TasksStatusPage</div>
+    <div className='flex h-full overflow-hidden'>
+      <NavBar />
+      <div style={{padding: '17px 18px'}} className='w-full'>
+        <h1 className='text-[20px] font-bold tracking-[0.03em] text-black opacity-95'>Eisenhower Metrix</h1>
+        <div className='w-full h-full grid grid-cols-2 grid-rows-2 gap-4.5' style={{marginTop: '8px'}}>
+          <UrgentTasks />
+          <NotUrgentTasks />
+          <ImportantTasks />
+          <UnimportantTasks />
+        </div>
+      </div>
+    </div>
   )
 }
 
